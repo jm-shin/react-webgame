f13fb5360b58be43e309190071a8be2e6d2a135b
jongmin <ace4463@gmail.com>
2022-02-21 17:15:22 +0900

    build: set webpack

---
+++ 2.끝말잇기/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+    name: 'word-relay-dev',
+    mode: 'development',
+    devtool: 'inline-source-map',
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+    entry: {
+        app: ['./client'],
+    },//입력
+    module: {
+        rules: [{
+            test: /\.jsx?/,
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/preset-env', '@babel/preset-react'],
+            },
+        }]
+    },
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'app.js'
+    },//출력
+};
